c9827d7edf90f9d31dda916d79a211b4f0f485d7
Simple and Tiny Utilities <yudai-tiny-developer@gmail.com>
2026-03-26 10:28:17 +0900

    v1.6.2

--- popup.js
+++ popup.js
@@ -22,6 +22,7 @@ function main(common, settings, progress, data) {
     const container3 = document.querySelector('div#container3');
     const container4 = document.querySelector('div#container4');
     const container5 = document.querySelector('div#container5');
+    const container6 = document.querySelector('div#container6');
     const reset_button = document.querySelector('input#reset');
     const progress_div = document.querySelector('div#reset_progress');
 
@@ -104,43 +105,45 @@ function main(common, settings, progress, data) {
         row.appendChild(settings.createLabel(cell_class, common.label.tooltip));
         row.appendChild(settings.createToggle(cell_class, toggle_class, label_class, 'tooltip_enabled', data.tooltip_enabled, common.default_tooltip_enabled, common.value));
         container3.appendChild(row);
-    } {
+    }
+
+    {
         const row = settings.createRow(row_class);
         row.appendChild(settings.createLabel(cell_class, common.label.youtube));
         row.appendChild(settings.createToggle(cell_class, toggle_class, label_class, 'youtube_enabled', data.youtube_enabled, common.default_youtube_enabled, common.value));
-        container3.appendChild(row);
+        container4.appendChild(row);
     } {
         const row = settings.createRow(row_class);
         row.appendChild(settings.createLabel(cell_class, common.label.more_videos));
         row.appendChild(settings.createToggle(cell_class, toggle_class, label_class, 'more_videos_enabled', data.more_videos_enabled, common.default_more_videos_enabled, common.value));
-        container3.appendChild(row);
+        container4.appendChild(row);
     } {
         const row = settings.createRow(row_class);
         row.appendChild(settings.createLabel(cell_class, common.label.share));
         row.appendChild(settings.createToggle(cell_class, toggle_class, label_class, 'share_enabled', data.share_enabled, common.default_share_enabled, common.value));
-        container3.appendChild(row);
+        container4.appendChild(row);
     }
 
     {
         const row = settings.createRow(row_class);
         row.appendChild(settings.createLabel(cell_class, common.label.search_voice));
         row.appendChild(settings.createToggle(cell_class, toggle_class, label_class, 'search_voice_enabled', data.search_voice_enabled, common.default_search_voice_enabled, common.value));
-        container4.appendChild(row);
+        container5.appendChild(row);
     } {
         const row = settings.createRow(row_class);
         row.appendChild(settings.createLabel(cell_class, common.label.create));
         row.appendChild(settings.createToggle(cell_class, toggle_class, label_class, 'create_enabled', data.create_enabled, common.default_create_enabled, common.value));
-        container4.appendChild(row);
+        container5.appendChild(row);
     } {
         const row = settings.createRow(row_class);
         row.appendChild(settings.createLabel(cell_class, common.label.notifications));
         row.appendChild(settings.createToggle(cell_class, toggle_class, label_class, 'notifications_enabled', data.notifications_enabled, common.default_notifications_enabled, common.value));
-        container4.appendChild(row);
+        container5.appendChild(row);
     } {
         const row = settings.createRow(row_class);
         row.appendChild(settings.createLabel(cell_class, common.label.account));
         row.appendChild(settings.createToggle(cell_class, toggle_class, label_class, 'account_enabled', data.account_enabled, common.default_account_enabled, common.value));
-        container4.appendChild(row);
+        container5.appendChild(row);
     }
 
     // Home & Shorts Section
@@ -148,17 +151,17 @@ function main(common, settings, progress, data) {
         const row = settings.createRow(row_class);
         row.appendChild(settings.createLabel(cell_class, common.label.sidebar_home_shorts_section));
         row.appendChild(settings.createToggle(cell_class, toggle_class, label_class, 'sidebar_home_shorts_section_enabled', data.sidebar_home_shorts_section_enabled, common.default_sidebar_home_shorts_section_enabled, common.value));
-        container5.appendChild(row);
+        container6.appendChild(row);
     } {
         const row = settings.createRow(row_class);
         row.appendChild(settings.createLabel(cell_class, common.label.sidebar_home));
         row.appendChild(settings.createToggle(cell_class, toggle_class, label_class, 'sidebar_home_enabled', data.sidebar_home_enabled, common.default_sidebar_home_enabled, common.value));
-        container5.appendChild(row);
+        container6.appendChild(row);
     } {
         const row = settings.createRow(row_class);
         row.appendChild(settings.createLabel(cell_class, common.label.sidebar_shorts));
         row.appendChild(settings.createToggle(cell_class, toggle_class, label_class, 'sidebar_shorts_enabled', data.sidebar_shorts_enabled, common.default_sidebar_shorts_enabled, common.value));
-        container5.appendChild(row);
+        container6.appendChild(row);
     }
 
     // Subscriptions Section
@@ -166,17 +169,17 @@ function main(common, settings, progress, data) {
         const row = settings.createRow(row_class);
         row.appendChild(settings.createLabel(cell_class, common.label.sidebar_subscriptions_section));
         row.appendChild(settings.createToggle(cell_class, toggle_class, label_class, 'sidebar_subscriptions_section_enabled', data.sidebar_subscriptions_section_enabled, common.default_sidebar_subscriptions_section_enabled, common.value));
-        container5.appendChild(row);
+        container6.appendChild(row);
     } {
         const row = settings.createRow(row_class);
         row.appendChild(settings.createLabel(cell_class, common.label.sidebar_subscriptions));
         row.appendChild(settings.createToggle(cell_class, toggle_class, label_class, 'sidebar_subscriptions_enabled', data.sidebar_subscriptions_enabled, common.default_sidebar_subscriptions_enabled, common.value));
-        container5.appendChild(row);
+        container6.appendChild(row);
     } {
         const row = settings.createRow(row_class);
         row.appendChild(settings.createLabel(cell_class, common.label.sidebar_subscriptions_channels));
         row.appendChild(settings.createToggle(cell_class, toggle_class, label_class, 'sidebar_subscriptions_channels_enabled', data.sidebar_subscriptions_channels_enabled, common.default_sidebar_subscriptions_channels_enabled, common.value));
-        container5.appendChild(row);
+        container6.appendChild(row);
     }
 
     // Library Section
@@ -184,52 +187,52 @@ function main(common, settings, progress, data) {
         const row = settings.createRow(row_class);
         row.appendChild(settings.createLabel(cell_class, common.label.sidebar_library_section));
         row.appendChild(settings.createToggle(cell_class, toggle_class, label_class, 'sidebar_library_section_enabled', data.sidebar_library_section_enabled, common.default_sidebar_library_section_enabled, common.value));
-        container5.appendChild(row);
+        container6.appendChild(row);
     } {
         const row = settings.createRow(row_class);
         row.appendChild(settings.createLabel(cell_class, common.label.sidebar_library));
         row.appendChild(settings.createToggle(cell_class, toggle_class, label_class, 'sidebar_library_enabled', data.sidebar_library_enabled, common.default_sidebar_library_enabled, common.value));
-        container5.appendChild(row);
+        container6.appendChild(row);
     } {
         const row = settings.createRow(row_class);
         row.appendChild(settings.createLabel(cell_class, common.label.sidebar_history));
         row.appendChild(settings.createToggle(cell_class, toggle_class, label_class, 'sidebar_history_enabled', data.sidebar_history_enabled, common.default_sidebar_history_enabled, common.value));
-        container5.appendChild(row);
+        container6.appendChild(row);
     } {
         const row = settings.createRow(row_class);
         row.appendChild(settings.createLabel(cell_class, common.label.sidebar_playlists));
         row.appendChild(settings.createToggle(cell_class, toggle_class, label_class, 'sidebar_playlists_enabled', data.sidebar_playlists_enabled, common.default_sidebar_playlists_enabled, common.value));
-        container5.appendChild(row);
+        container6.appendChild(row);
     } {
         const row = settings.createRow(row_class);
         row.appendChild(settings.createLabel(cell_class, common.label.sidebar_watch_later));
         row.appendChild(settings.createToggle(cell_class, toggle_class, label_class, 'sidebar_watch_later_enabled', data.sidebar_watch_later_enabled, common.default_sidebar_watch_later_enabled, common.value));
-        container5.appendChild(row);
+        container6.appendChild(row);
     } {
         const row = settings.createRow(row_class);
         row.appendChild(settings.createLabel(cell_class, common.label.sidebar_liked_videos));
         row.appendChild(settings.createToggle(cell_class, toggle_class, label_class, 'sidebar_liked_videos_enabled', data.sidebar_liked_videos_enabled, common.default_sidebar_liked_videos_enabled, common.value));
-        container5.appendChild(row);
+        container6.appendChild(row);
     } {
         const row = settings.createRow(row_class);
         row.appendChild(settings.createLabel(cell_class, common.label.sidebar_your_videos));
         row.appendChild(settings.createToggle(cell_class, toggle_class, label_class, 'sidebar_your_videos_enabled', data.sidebar_your_videos_enabled, common.default_sidebar_your_videos_enabled, common.value));
-        container5.appendChild(row);
+        container6.appendChild(row);
     } {
         const row = settings.createRow(row_class);
         row.appendChild(settings.createLabel(cell_class, common.label.sidebar_downloads));
         row.appendChild(settings.createToggle(cell_class, toggle_class, label_class, 'sidebar_downloads_enabled', data.sidebar_downloads_enabled, common.default_sidebar_downloads_enabled, common.value));
-        container5.appendChild(row);
+        container6.appendChild(row);
     } {
         const row = settings.createRow(row_class);
         row.appendChild(settings.createLabel(cell_class, common.label.sidebar_podcasts));
         row.appendChild(settings.createToggle(cell_class, toggle_class, label_class, 'sidebar_podcasts_enabled', data.sidebar_podcasts_enabled, common.default_sidebar_podcasts_enabled, common.value));
-        container5.appendChild(row);
+        container6.appendChild(row);
     } {
         const row = settings.createRow(row_class);
         row.appendChild(settings.createLabel(cell_class, common.label.sidebar_clips));
         row.appendChild(settings.createToggle(cell_class, toggle_class, label_class, 'sidebar_clips_enabled', data.sidebar_clips_enabled, common.default_sidebar_clips_enabled, common.value));
-        container5.appendChild(row);
+        container6.appendChild(row);
     }
 
     // Explore Section
@@ -237,7 +240,7 @@ function main(common, settings, progress, data) {
         const row = settings.createRow(row_class);
         row.appendChild(settings.createLabel(cell_class, common.label.sidebar_explore_section));
         row.appendChild(settings.createToggle(cell_class, toggle_class, label_class, 'sidebar_explore_section_enabled', data.sidebar_explore_section_enabled, common.default_sidebar_explore_section_enabled, common.value));
-        container5.appendChild(row);
+        container6.appendChild(row);
     }
 
     // More from YouTube Section
@@ -245,36 +248,36 @@ function main(common, settings, progress, data) {
         const row = settings.createRow(row_class);
         row.appendChild(settings.createLabel(cell_class, common.label.sidebar_more_from_youtube_section));
         row.appendChild(settings.createToggle(cell_class, toggle_class, label_class, 'sidebar_more_from_youtube_section_enabled', data.sidebar_more_from_youtube_section_enabled, common.default_sidebar_more_from_youtube_section_enabled, common.value));
-        container5.appendChild(row);
+        container6.appendChild(row);
     } {
         const row = settings.createRow(row_class);
         row.appendChild(settings.createLabel(cell_class, common.label.sidebar_youtube_premium));
         row.appendChild(settings.createToggle(cell_class, toggle_class, label_class, 'sidebar_youtube_premium_enabled', data.sidebar_youtube_premium_enabled, common.default_sidebar_youtube_premium_enabled, common.value));
-        container5.appendChild(row);
+        container6.appendChild(row);
     } {
         const row = settings.createRow(row_class);
         row.appendChild(settings.createLabel(cell_class, common.label.sidebar_youtube_music));
         row.appendChild(settings.createToggle(cell_class, toggle_class, label_class, 'sidebar_youtube_music_enabled', data.sidebar_youtube_music_enabled, common.default_sidebar_youtube_music_enabled, common.value));
-        container5.appendChild(row);
+        container6.appendChild(row);
     } {
         const row = settings.createRow(row_class);
         row.appendChild(settings.createLabel(cell_class, common.label.sidebar_youtube_kids));
         row.appendChild(settings.createToggle(cell_class, toggle_class, label_class, 'sidebar_youtube_kids_enabled', data.sidebar_youtube_kids_enabled, common.default_sidebar_youtube_kids_enabled, common.value));
-        container5.appendChild(row);
+        container6.appendChild(row);
     }
 
     {
         const row = settings.createRow(row_class);
         row.appendChild(settings.createLabel(cell_class, common.label.sidebar_report_history));
         row.appendChild(settings.createToggle(cell_class, toggle_class, label_class, 'sidebar_report_history_enabled', data.sidebar_report_history_enabled, common.default_sidebar_report_history_enabled, common.value));
-        container5.appendChild(row);
+        container6.appendChild(row);
     }
 
     {
         const row = settings.createRow(row_class);
         row.appendChild(settings.createLabel(cell_class, common.label.sidebar_about));
         row.appendChild(settings.createToggle(cell_class, toggle_class, label_class, 'sidebar_about_enabled', data.sidebar_about_enabled, common.default_sidebar_about_enabled, common.value));
-        container5.appendChild(row);
+        container6.appendChild(row);
     }
 
     settings.registerResetButton(reset_button, progress_div, progress_class, done_class, toggle_class, input_class, progress);
